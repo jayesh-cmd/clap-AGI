e963a18374f8797691ecf494234c32121f7323a6
Jayesh <jayesh@example.com>
2026-04-10 12:34:51 +0530

    Few

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import numpy as np
 import time
 import os
 
-# --- Configuration ---
 THRESHOLD = 3000       # How loud a sound needs to be to register as a "clap". Adjust if it's too sensitive or not sensitive enough.
 MIN_DELAY = 0.2        # Minimum seconds between claps to avoid echoing registering as a second clap.
 MAX_DELAY = 1.0        # Maximum seconds between claps. If you wait longer, it resets.
 
-# --- Audio Setup ---
 CHUNK = 1024           # Number of audio frames per buffer
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -49,21 +47,14 @@ try:
                 if clap_count == 2:
                     print("Double clap confirmed! Initiating Workspace Protocol...")
                     
-                    # 1. Start the Music FIRST
-                    # Swap this link with your actual YouTube video or playlist URL
-                    os.system("open -a 'Google Chrome' 'https://youtu.be/xMaE6toi4mk?si=eExFvxFs4sZeCR6w'")
-                    
-                    # Add a tiny 2-second pause to let the browser load the video
+
+                    # Add Tasks Here 
+
                     time.sleep(2) 
                     
-                    # 3. Open Gemini
-                    os.system("open -a 'Google Chrome' 'https://gemini.google.com'")
-
-                    # 3. Open a specific project in VS Code (replace the path with your actual folder path)
-                    os.system("open -a 'Visual Studio Code' '/Users/jayeshvishwakarma/Desktop/Daydreamin/Server/app.py'")
+                    # Like here i am opening chrome and the link to directly open my youtube channel
+                    os.system("open -a 'Google Chrome' https://youtube.com/@jey_script?si=nxld3d9gwCj54RGC")
                     
-                    # 4. Open the Claude Mac app LAST so it is the active window
-                    os.system("open -a 'Claude'")
                     
                     clap_count = 0
                     
